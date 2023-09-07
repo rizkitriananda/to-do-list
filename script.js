a73aaab5ac256801ab3c5f52e4e3bed7502a0c9d
rizkitriananda <riskitriananda321@gmail.com>
2023-09-07 10:13:14 +0700

    frist commit

--- script.js
+++ script.js
@@ -6,14 +6,27 @@ btnTambah.addEventListener("click", function (e) {
   e.preventDefault();
   const li = document.createElement("li");
   const remove = document.createElement("i");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
   remove.classList.add("fas", "fa-xmark");
   li.textContent = inputText.value;
   ul.appendChild(li);
+  li.append(checkbox);
   inputText.value = "";
 
   li.appendChild(remove);
 
   remove.addEventListener("click", function () {
     ul.removeChild(li);
+  });
+
+  checkbox.addEventListener("change", function () {
+    if (checkbox.checked) {
+      li.style.textDecoration = "line-through";
+      li.style.color = "grey";
+    } else {
+      li.style.textDecoration = "none";
+      li.style.color = "#111111";
+    }
   });
 });
